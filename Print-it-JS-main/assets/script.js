5359c5bc901a4496a210f560765ab2bfacb38beb
Redwan <moody.d78190>
2023-12-28 13:20:17 +0100

    realisation and implementation of the carrousel

---
+++ Print-it-JS-main/assets/script.js
@@ -0,0 +1,71 @@
+const slides = [
+	{
+	  "image": "slide1.jpg",
+	  "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+	  "image": "slide2.jpg",
+	  "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+	  "image": "slide3.jpg",
+	  "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+	  "image": "slide4.png",
+	  "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+  ];
+  
+  const leftArrow = document.querySelector(".arrow_left");
+  const rightArrow = document.querySelector(".arrow_right");
+  const image = document.getElementById("image");
+  const tagline = document.getElementById("tagline");
+  const dots = document.querySelectorAll('.dot');
+  
+  let currentSlideIndex = 0;
+  displaySlide();
+  
+  function displaySlide() {
+	let slide = slides[currentSlideIndex];
+	image.src = "./assets/images/slideshow/" + slide.image;
+	tagline.innerHTML = slide.tagLine;
+  
+	// Mettre à jour les dots pour indiquer le slide actuel
+	dots.forEach((dot, index) => {
+	  if (index === currentSlideIndex) {
+		dot.classList.add('dot_selected');
+	  } else {
+		dot.classList.remove('dot_selected');
+	  }
+	});
+  }
+  
+  leftArrow.addEventListener('click', () => {
+	if (currentSlideIndex === 0) {
+	  currentSlideIndex = slides.length - 1;
+	  displaySlide();
+	} else {
+	  currentSlideIndex--;
+	  displaySlide();
+	}
+  });
+  
+  rightArrow.addEventListener('click', () => {
+	if (currentSlideIndex === slides.length - 1) {
+	  currentSlideIndex = 0;
+	  displaySlide();
+	} else {
+	  currentSlideIndex++;
+	  displaySlide();
+	}
+  });
+  
+  // Ajout d'écouteurs d'événements pour les dots
+  dots.forEach((dot, index) => {
+	dot.addEventListener('click', () => {
+	  currentSlideIndex = index;
+	  displaySlide();
+	});
+  });
+  
